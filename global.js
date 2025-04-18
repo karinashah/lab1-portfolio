8c13e7217e6cf4d5c0b0a41f6b7b14acba86e3fb
Karina Shah <k7shah@ucsd.edu>
2025-04-17 23:42:04 -0700

    more

--- global.js
+++ global.js
@@ -1,30 +1,32 @@
-console.log('IT’S ALIVE!');
-
 let pages = [
   { url: '', title: 'Home' },
   { url: 'projects/', title: 'Projects' },
-  { url: 'contact/', title: 'Contact' },
-  { url: 'resume/', title: 'Resume' },
 ];
-
-const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-  ? "/"                  
-  : "/website/";         
 
 let nav = document.createElement('nav');
 document.body.prepend(nav);
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
 
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
 
-  if (!url.startsWith('http')) {
-    url = !url.startsWith('http') ? BASE_PATH + url : url;
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  nav.append(a);
+
+  a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname
+  );
+
+  if (a.host !== location.host) {
+    a.target = "_blank";
   }
-
-  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
-
-let navLinks = document.querySelectorAll('nav a');
-let currentLink = [...navLinks].find(a => a.href === location.href);
-currentLink?.classList.add('current');
