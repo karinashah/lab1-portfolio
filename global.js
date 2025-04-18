54e3439de30231586d6fd317918ad6221868d3b0
Karina Shah <k7shah@ucsd.edu>
2025-04-17 23:05:45 -0700

    removing nav pages

--- global.js
+++ global.js
@@ -1,5 +1,30 @@
 console.log('IT’S ALIVE!');
 
-function $$(selector, context = document) {
-  return Array.from(context.querySelectorAll(selector));
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume' },
+];
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  
+  : "/website/";         
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  if (!url.startsWith('http')) {
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+  }
+
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
+
+let navLinks = document.querySelectorAll('nav a');
+let currentLink = [...navLinks].find(a => a.href === location.href);
+currentLink?.classList.add('current');
