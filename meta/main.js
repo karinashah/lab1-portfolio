e22eb43838b4dcd5c874da46b3ce410d80986f0c
Karina Shah <k7shah@ucsd.edu>
2025-05-10 00:03:45 -0700

    adding axis

--- meta/main.js
+++ meta/main.js
@@ -93,6 +93,69 @@ function renderCommitInfo(data, commits) {
     dl.append('dd').text(`${busiestHour}:00`);
   }
 
-renderCommitInfo(data, commits);
+  function renderScatterPlot(data, commits) {
+    const width = 1000;
+    const height = 600;
+    const margin = { top: 10, right: 10, bottom: 30, left: 40 };
+  
+    const usableArea = {
+      top: margin.top,
+      right: width - margin.right,
+      bottom: height - margin.bottom,
+      left: margin.left,
+      width: width - margin.left - margin.right,
+      height: height - margin.top - margin.bottom,
+    };
+  
+    const svg = d3
+      .select('#chart')
+      .append('svg')
+      .attr('viewBox', `0 0 ${width} ${height}`)
+      .style('overflow', 'visible');
+  
+    const xScale = d3
+      .scaleTime()
+      .domain(d3.extent(commits, d => d.datetime))
+      .range([usableArea.left, usableArea.right])
+      .nice();
+  
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, 24])
+      .range([usableArea.bottom, usableArea.top]);
+  
+    // Draw dots
+    const dots = svg.append('g').attr('class', 'dots');
+  
+    dots
+      .selectAll('circle')
+      .data(commits)
+      .join('circle')
+      .attr('cx', d => xScale(d.datetime))
+      .attr('cy', d => yScale(d.hourFrac))
+      .attr('r', 5)
+      .attr('fill', 'steelblue');
+  
+    // Axes
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale)
+      .tickFormat(d => String(d % 24).padStart(2, '0') + ':00');
+  
+    // Add X axis
+    svg
+      .append('g')
+      .attr('transform', `translate(0, ${usableArea.bottom})`)
+      .call(xAxis);
+  
+    // Add Y axis
+    svg
+      .append('g')
+      .attr('transform', `translate(${usableArea.left}, 0)`)
+      .call(yAxis);
+  }  
+
+  renderCommitInfo(data, commits);
+  renderScatterPlot(data, commits);
+
 
   
