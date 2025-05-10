92368c1bb400ae266b33e73bf95185fd1c942ce5
Karina Shah <k7shah@ucsd.edu>
2025-05-09 23:50:11 -0700

    adding main.js

---
+++ meta/main.js
@@ -0,0 +1,47 @@
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
+async function loadData() {
+  const data = await d3.csv('loc.csv', (row) => ({
+    ...row,
+    line: +row.line,
+    depth: +row.depth,
+    length: +row.length,
+    date: new Date(row.date + 'T00:00' + row.timezone),
+    datetime: new Date(row.datetime),
+  }));
+  return data;
+}
+
+function processCommits(data) {
+  return d3
+    .groups(data, d => d.commit)
+    .map(([commit, lines]) => {
+      let first = lines[0];
+      let { author, date, time, timezone, datetime } = first;
+
+      let ret = {
+        id: commit,
+        url: 'https://github.com/vis-society/lab-7/commit/' + commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length,
+      };
+
+      Object.defineProperty(ret, 'lines', {
+        value: lines,
+        enumerable: false,
+        writable: false,
+        configurable: false,
+      });
+
+      return ret;
+    });
+}
+
+let data = await loadData();
+let commits = processCommits(data);
+console.log(commits);
